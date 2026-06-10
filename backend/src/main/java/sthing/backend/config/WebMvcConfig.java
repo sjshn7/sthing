1bf843353d2c46a9ca33010c54cb272491fc9b9a
sj <vviva0324@gmail.com>
2026-06-11 03:18:22 +0900

    배포 도메인 관련 수정

--- backend/src/main/java/sthing/backend/config/WebMvcConfig.java
+++ backend/src/main/java/sthing/backend/config/WebMvcConfig.java
@@ -16,7 +16,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://sthing-character.site"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
